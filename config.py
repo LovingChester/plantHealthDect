4be72cda1d3e6167843d55aa6cdf03e85d6cf80a
LovingChester <chelsea000719@gmail.com>
2024-03-06 11:33:00 -0600

    update config

--- config.py
+++ config.py
@@ -25,12 +25,12 @@ cfg.data.mode = 'train'
 cfg.train = edict()
 
 cfg.train.batch_size = 14
-cfg.train.learning_rate = 5e-4  # initial learning rate
+cfg.train.learning_rate = 0.01  # initial learning rate
 cfg.train.l2_reg = 0
 cfg.train.lr_decay = 0.9
 cfg.train.lr_decay_every = 3
 cfg.train.shuffle = True
-cfg.train.num_epochs = 100
+cfg.train.num_epochs = 1
 cfg.train.num_workers = 0
 
 cfg.train.out_dir = './outputs/dem_derovative1'
