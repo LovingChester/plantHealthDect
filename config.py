126880c4cb71df8969f085357da0c3cd14a3a982
LovingChester <chelsea000719@gmail.com>
2024-03-07 14:56:08 -0600

    add weights to config

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ cfg = edict()
 
 # Model
 cfg.model = edict()
-# 'vgg16'
+# 'vgg16', 'vit'
 cfg.model.name = 'vgg16'
 cfg.model.pre_trained = False
 
@@ -24,13 +24,21 @@ cfg.data.mode = 'train'
 # Training details
 cfg.train = edict()
 
-cfg.train.batch_size = 14
+cfg.train.batch_size = 16
 cfg.train.learning_rate = 0.01  # initial learning rate
 cfg.train.l2_reg = 0
 cfg.train.lr_decay = 0.9
 cfg.train.lr_decay_every = 3
 cfg.train.shuffle = True
-cfg.train.num_epochs = 1
-cfg.train.num_workers = 0
+cfg.train.num_epochs = 2
+cfg.train.num_workers = 2
 
-cfg.train.out_dir = './outputs/dem_derovative1'
+cfg.train.weights_without_aug = [44.01, 44.64, 100.81, 16.85, 24.26, 18.46, 32.46, 26.35, 54.04, 23.26, 23.86, 28.15, 
+                                23.49, 20.05, 65.54, 25.77, 5.03, 12.07, 77.01, 27.81, 18.76, 27.72, 182.39, 27.72, 
+                                74.73, 5.45, 15.11, 60.8, 25.0, 13.03, 27.72, 17.43, 14.52, 29.12, 15.65, 16.54, 19.75, 
+                                74.33, 5.18]
+cfg.train.weights_with_aug = [30.74, 30.74, 30.74, 18.69, 26.9, 20.47, 30.74, 29.22, 30.74, 25.79, 26.46, 30.74, 26.05, 
+                              22.23, 30.74, 28.57, 5.58, 13.38, 30.74, 30.74, 20.8, 30.74, 30.74, 30.74, 30.74, 6.04, 
+                              16.75, 30.74, 27.72, 14.45, 30.74, 19.32, 16.1, 30.74, 17.36, 18.34, 21.9, 30.74, 5.74]
+
+cfg.train.out_dir = './outputs'
